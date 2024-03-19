0dcf4f14b0170ef88d3968731741e82969a3ef60
Farhan Khan <farhankhan.ug21@nsut.ac.in>
2024-03-20 00:00:10 +0530

    first

---
+++ run_pipeline.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipeline import train_pipeline
+
+if(__name__ == "main"):
+    train_pipeline(data_path="C:\\Users\\Welcome\\Desktop\\Sem6\\CSCH\\DevOps\\main\\data\\olist_customers_dataset.csv")
